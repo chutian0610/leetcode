99b4d2b23a9241278f4a3b9eb759707053aceb0c
楚旻 <chumin.ct@alibaba-inc.com>
2020-04-21 14:39:00 +0800

    leetcode Question 6

---
+++ src/test/java/info/victorchu/q6/ZTransformerTest.java
@@ -0,0 +1,27 @@
+package info.victorchu.q6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZTransformerTest {
+
+    @Test
+    public void convert1() {
+        String s= "LEETCODEISHIRING";
+        int numRows = 4;
+        ZTransformer zTransformer = new ZTransformer();
+        String result = "LDREOEIIECIHNTSG";
+        Assert.assertEquals(result,zTransformer.convert(s,numRows));
+    }
+
+    @Test
+    public void convert2() {
+        String s= "LEETCODEISHIRING";
+        int numRows = 3;
+        ZTransformer zTransformer = new ZTransformer();
+        String result = "LCIRETOESIIGEDHN";
+        Assert.assertEquals(result,zTransformer.convert(s,numRows));
+    }
+}
